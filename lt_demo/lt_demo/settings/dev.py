54a2597cc6c50096659bcd707ab49fc51c4c8f25
xiangming <1435789273@qq.com>
2020-09-02 23:54:10 +0800

    创建注册demo子应用

--- lt_demo/lt_demo/settings/dev.py
+++ lt_demo/lt_demo/settings/dev.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# 获取apps路径
+apps_path = os.path.join(BASE_DIR, "apps")
+# 添加导包路径
+# print(apps_path)
+sys.path.insert(0, apps_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -35,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'demo',
 ]
 
 MIDDLEWARE = [
