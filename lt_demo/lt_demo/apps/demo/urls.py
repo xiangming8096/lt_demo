d1d6f148fa30baa4124ab077ff7999c6f7d1f0e4
xiangming8096 <lt1435789273@qq.com>
2020-09-04 16:43:10 +0800

    完整功能（未验证代码功能是否正常）

--- lt_demo/lt_demo/apps/demo/urls.py
+++ lt_demo/lt_demo/apps/demo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'^index/$', views.Index),
+    path(r'^province_areas/$', views.ProvinceAreasView),  # 省级地址列表
+    path(r'^sub_areas/$', views.SubAreasView),  # 市区级地址列表
+    path(r'^insert/$', views.InsertView),  # 添加数据
+
 ]
